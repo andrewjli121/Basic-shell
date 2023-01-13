b6fbf7284833e4de69c4663dcfb7c2a1f699f7c0
andrewjli121 <andrewjli121@gmail.com>
2023-01-12 21:33:25 -0600

    working

--- Shell/main.c
+++ Shell/main.c
@@ -1,19 +1,4 @@
-strdup_every_other_char(const char* s) {
-    int length = 0;
-    const char* b = s;
-    while(*s != NULL) {
-        length += 1;
-        s += 1;
-    }
-    char* result = new char[(length + 1) / 2];
-    s = b;
-    for(int i = 0; i < (length + 1) / 2; i++) {
-        result[i] = *s;
-        s += 2;
-    }
-};
-
 int main() {
 
-    printf(strdup_every_other_char("house"));
+    printf("hi");
 }

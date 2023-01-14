e2968aeab67bcbf99791fe1eb061ccf1cf779114
andrewjli121 <andrewjli121@gmail.com>
2023-01-13 19:44:47 -0600

    Begin

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+int main(int argc, char **argv){
+
+    int cpid, ret;
+    char *cmd;
+
+    while (1){
+		cmd = readline("> ");
+		cpid = fork();
+		if (cmd == NULL) break;
+		if (cpid ==0){
+			//Child
+			ret=execlp(cmd, cmd, NULL);
+			if (ret ==-1){
+			printf("Bad command\n");
+			}
+		}
+		wait(NULL);
+    }
+    printf("done\n");
+    exit(1);
+}
--- test.py
+++
@@ -1,9 +0,0 @@
-def function(input: str) -> str:
-    result = ''
-    for i in range(len(input)):
-        if i % 2 == 0:
-            result = result + input[i]
-    return result
-
-
-print(function('test'))

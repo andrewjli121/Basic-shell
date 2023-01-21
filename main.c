5bb127b7a29c8e2b0c36c65eca361f95ab674fc3
andrewjli121 <andrewjli121@gmail.com>
2023-01-20 18:10:01 -0600

    ps

--- main.c
+++ main.c
@@ -9,13 +9,20 @@
 
 int main(int argc, char **argv){
 
-    int cpid, ret;
+    int cpid, ret, fd;
+	int ps_flag = 0;
     char *cmd;
 	char **parsedcmd;
+	char **ps_parse;
 
     while (cmd = readline("> ")) {
-
-		parsedcmd = parse(cmd, ' ');
+		ps_flag = 0;
+		parsedcmd = parse(cmd, ' ', ps_parse, &ps_flag);
+		if(ps_flag) {
+			for(int i = 0; ps_parse[i] != NULL; i++) {
+				printf("%i\n", i);
+			}
+		}
 		cpid = fork();
 		if (cpid ==0){
 			//Child
@@ -26,6 +33,7 @@ int main(int argc, char **argv){
 		} else{
 			wait((int *)NULL);
 		}
+
     }
     printf("done\n");
     exit(1);
--- parser.c
+++ parser.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-char** parse(char* str, char delim)  {
+void check_ps(char* str, char delim, char** saveptr, char** ps_parse);
+
+char** parse(char* str, char delim, char** ps_parse, int* ps_flag)  {
     int length = strlen(str);
     char** result = malloc(length * sizeof(char*));
     char* saveptr;
@@ -10,10 +12,32 @@ char** parse(char* str, char delim)  {
     int i = 0;
     for(str; ; str = NULL) {
         subtoken = strtok_r(str, &delim, &saveptr);
+        length--;
         if(subtoken == NULL) break;
-        *(result+i) = subtoken;
+        if(strcmp(subtoken, "<")==0 || strcmp(subtoken, "<")==0 || strcmp(subtoken, "|") ==0 || strcmp(subtoken, "2<")==0) {
+            ps_parse = malloc(length * sizeof(char*));
+            *ps_parse = subtoken;
+            check_ps(str, delim, &saveptr, ps_parse);
+            *ps_flag = 1;
+            break;
+        }
+        result[i] = subtoken;
         i++;
     }
-    *(result+i) = NULL;
+    result[i] = NULL;
     return result;
+}
+
+void check_ps(char* str, char delim, char** saveptr, char** ps_parse) {
+    int i = 1;
+    char* subtoken;
+    for(str; ; str = NULL) {
+        subtoken = strtok_r(str, &delim, saveptr);
+        if(subtoken == NULL) break;
+        ps_parse[i] = subtoken;
+        printf("%s\n", subtoken);
+        i++;
+    }
+    ps_parse[i] = NULL;
+    return;
 }

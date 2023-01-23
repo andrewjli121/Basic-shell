30d7eeb1760d538ed0c046a12d0a66e33e7e2833
andrewjli121 <andrewjli121@gmail.com>
2023-01-23 11:37:59 -0600

    Redirection + piping finish

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 
 int main(int argc, char **argv){
 
-    int cpid_l, cpid_r, ret, fd;
+    int cpid_l, cpid_r, ret, fd, fd2;
 	int ps_flag = 0;
     char *cmd;
 	char **parsedcmd;
@@ -34,43 +34,72 @@ int main(int argc, char **argv){
 				//pipe
 				char* op = ps_parse[i];
 				char* file = ps_parse[i+1];
-				if(strcmp(op, "|") == 0) {
-						pipe(pfd);
-						cpid_l = fork();
-						if(cpid_l == 0) {
-							close(pfd[0]);
-							dup2(pfd[1],1);
-							close(pfd[1]);
-							if(i == 0) {
-								ret = execvp(parsedcmd[0], parsedcmd);
-								printf("ran");
-							}
-							else {
-								char* command = ps_parse[i-1];
-								ret = execlp(command,command,NULL);
-							}
-							if (ret ==-1){
-								//fprintf(stderr, "Bad command\n");
-								exit(1);
-							}
+				pipe(pfd);
+				cpid_l = fork();
+				if(cpid_l == 0) {
+					close(pfd[0]);
+					dup2(pfd[1],1);
+					close(pfd[1]);
+					while(strcmp(ps_parse[i], "|") != 0) {
+						char* op = ps_parse[i];
+						char* file = ps_parse[i+1];
+						if(strcmp(op, "<") == 0) {
+							fclose(fopen(file, "a"));
+							fd = open(file, O_RDONLY);
+							dup2(fd, 0);
+						}else if(strcmp(op, ">") == 0) {
+							fclose(fopen(file, "a"));
+							fd = open(file, O_WRONLY);
+							dup2(fd, 1);
+						} else if(strcmp(op, "2>") == 0) {
+							fclose(fopen(file, "a"));
+							fd = open(file, O_WRONLY);
+							dup2(fd, 2);
 						}
-						cpid_r = fork();
-						if(cpid_r == 0) {
-							char* command = ps_parse[i+1];
-							close(pfd[1]);
-							dup2(pfd[0],0);
-							close(pfd[0]);
-							ret = execlp(command, command, NULL);
-							if (ret ==-1){
-								//fprintf(stderr, "Bad command\n");
-								exit(1);
-							}
-						}
-						close(pfd[0]);
-						close(pfd[1]);
-						wait(NULL);
-						wait(NULL);
+						i += 2;
+					}
+					ret = execvp(parsedcmd[0], parsedcmd);
+					if (ret ==-1){
+						//fprintf(stderr, "Bad command\n");
+						exit(1);
+					}
 				}
+				cpid_r = fork();
+				if(cpid_r == 0) {
+					char* command = ps_parse[i+3];
+					i += 4;
+					close(pfd[1]);
+					dup2(pfd[0],0);
+					close(pfd[0]);
+					while(ps_parse[i] != NULL) {
+						char* op = ps_parse[i];
+						char* file = ps_parse[i+1];
+						if(strcmp(op, "<") == 0) {
+							fclose(fopen(file, "a"));
+							fd2 = open(file, O_RDONLY);
+							dup2(fd2, 0);
+						}else if(strcmp(op, ">") == 0) {
+							fclose(fopen(file, "a"));
+							fd2 = open(file, O_WRONLY);
+							dup2(fd2, 1);
+						} else if(strcmp(op, "2>") == 0) {
+							fclose(fopen(file, "a"));
+							fd2 = open(file, O_WRONLY);
+							dup2(fd2, 2);
+						}
+						i += 2;
+					}
+					printf("%s", command);
+					ret = execlp(command, command, NULL);
+					if (ret ==-1){
+						//fprintf(stderr, "Bad command\n");
+						exit(1);
+					}
+				}
+				close(pfd[0]);
+				close(pfd[1]);
+				wait(NULL);
+				wait(NULL);
 			} else {
 				//no pipe
 				cpid_l = fork();
